1b2d2f42423db7795388a1a23938337059b9f642
Muhammad Umair Tahir <nobelhighschool@hotmail.com>
2020-02-02 21:20:19 +0500

    removed extra method

--- ModelRepositoryFactory/IDataModel.cs
+++ ModelRepositoryFactory/IDataModel.cs
@@ -6,7 +6,6 @@ namespace EntityGrabber
     public interface IDataModel<T>
     {
         T Id { get; set; }
-        Type GetIdType();
     }
 #pragma warning restore CS1591 // Missing XML comment for publicly visible type or member
 }
